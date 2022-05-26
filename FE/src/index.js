1b92712ffc208cba81992b07e976c6cb94f1fef5
Hao Wu <32400236+haowu84@users.noreply.github.com>
2022-05-26 03:12:03 -0700

    Update index.js

--- FE/src/index.js
+++ FE/src/index.js
@@ -6,15 +6,7 @@ import App from 'app/App';
 import "index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-/**
- * This is the starting point of our application!
- *
- * Here we have our <App> (Think of this as the root of your application)
- *
- * You'll notice its wrapped inside a <BrowserRouter>. This will allow us
- * to navigate around our site by switching which component the user sees
- * depending on the url of the website.
- */
+
 ReactDOM.render(
     <BrowserRouter>
         <App/>
